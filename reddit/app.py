daaba6e253d636a820eb1597756c766bcf6c2d09
Mate Mijolovic <mate.mijolovic@fer.hr>
2019-11-26 23:57:32 +0100

    Cleanup imports

--- reddit/app.py
+++ reddit/app.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import render_template, Flask
+from flask import Flask
 
 from reddit.models import User, Subreddit, Post, Comment
 from reddit.security import login_manager, bcrypt
